b7e4b0dc59109de728cdb76b0851f6c73ca13eee
vani0432 <grandhivani@gmail.com>
2024-04-15 09:55:17 +0530

    project

---
+++ mongodb.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "scripted started excuted in $TIMESTAMP" &>> $LOGFILE
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR: $2 ...... $R FAILED $N"
+        exit 1
+    else
+        echo "$2 ... $G SUCCESS $N"
+    fi
+
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR: your are not root user"
+    exit 1
+else
+    echo "your root user"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo  &>> $LOGFILE
+
+VALIDATE $? "copied mongodb repo"
